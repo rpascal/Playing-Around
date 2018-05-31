add71cef9367b9bda4394495260d95f2a556d6a4
Ryan Pascal Desktop <ryanpascal99@yahoo.com>
2018-05-30 21:23:41 -0400

    Code Cleanup

--- Assets/Prefabs/Enemy/EnemyCollision.cs
+++ Assets/Prefabs/Enemy/EnemyCollision.cs
@@ -5,26 +5,16 @@ using UnityEngine;
 public class EnemyCollision : MonoBehaviour {
 
 
-
-    // Use this for initialization
     void Start() {
 
     }
 
-    // Update is called once per frame
     void Update() {
         transform.Translate(Vector3.left * 1f * Time.deltaTime);
     }
 
     private void OnParticleCollision(GameObject other) {
-        print(other);
         Destroy(gameObject);
-    }
-
-
-    private void OnParticleTrigger() {
-        print("OnParticleTrigger");
-
     }
 
 }
---
+++ Assets/Prefabs/Player/CubeMovement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMovement : MonoBehaviour {
+
+    [SerializeField] ParticleSystem ParticleSystem;
+    [SerializeField] float speed = 10f;
+
+
+    Rigidbody playerRigidbody;
+    ParticleSystem.EmissionModule emissionModule;
+    Vector3 currentDirection;
+
+    void Start() {
+        playerRigidbody = GetComponent<Rigidbody>();
+        emissionModule = ParticleSystem.emission;
+        currentDirection = Vector3.forward;
+    }
+
+
+    void Update() {
+        UpdateDirection();
+        DisplayTrail();
+        Move();
+    }
+
+    private void Move() {
+        var movement = currentDirection * speed * Time.deltaTime;
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+
+    private void DisplayTrail() {
+        if (Input.GetKey(KeyCode.Space)) {
+            emissionModule.enabled = true;
+        } else {
+            emissionModule.enabled = false;
+        }
+    }
+
+    private void UpdateDirection() {
+        if (isChangingDirection(KeyCode.W, Vector3.forward)) {
+            currentDirection = Vector3.forward;
+        } else if (isChangingDirection(KeyCode.A, Vector3.left)) {
+            currentDirection = Vector3.left;
+        } else if (isChangingDirection(KeyCode.S, Vector3.back)) {
+            currentDirection = Vector3.back;
+        } else if (isChangingDirection(KeyCode.D, Vector3.right)) {
+            currentDirection = Vector3.right;
+        }
+    }
+
+    private bool isChangingDirection(KeyCode key, Vector3 direction) {
+        return (Input.GetKeyDown(key) && currentDirection != direction);
+    }
+
+}
